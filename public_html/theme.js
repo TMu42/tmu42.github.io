e3a3e0acfcc82737036de98b0e6bdb0468d49d62
TMu42 <11731869+TMu42@users.noreply.github.com>
2024-08-29 10:52:36 +0000

    Light-Dark-Auto theme controller implemented with buttons in drop down.

--- public_html/theme.js
+++ public_html/theme.js
@@ -5,9 +5,20 @@ const dark = window.matchMedia("(prefers-color-scheme: dark)");
 const titIm0 = document.getElementById("title-image0");
 const titIm1 = document.getElementById("title-image1");
 
-const updateTheme = () => {
-    
-    if(dark.matches) {
+const lightButton = document.getElementById("light-button");
+const  autoButton = document.getElementById( "auto-button");
+const  darkButton = document.getElementById( "dark-button");
+
+
+lightButton.addEventListener("click", changeTheme);
+autoButton.addEventListener("click", changeTheme);
+darkButton.addEventListener("click", changeTheme);
+
+var mode = "auto";
+
+
+function updateTheme() {
+    if(mode == "dark" || (mode == "auto" && dark.matches)) {
         favLnk.href = "./favicon-white.png";
         titIm0.src = "./favicon-light.png";
         titIm1.src = "./favicon-light.png";
@@ -17,6 +28,44 @@ const updateTheme = () => {
         titIm1.src = "./favicon-dark.png";
     }
 };
+
+
+function changeTheme(event) {
+    const  darkChild = [ darkButton, ...getDescendants( darkButton)];
+    const  autoChild = [ autoButton, ...getDescendants( autoButton)];
+    const lightChild = [lightButton, ...getDescendants(lightButton)];
+    
+    darkButton.style.removeProperty("background-color");
+    autoButton.style.removeProperty("background-color");
+    lightButton.style.removeProperty("background-color");
+    
+    if(darkChild.includes(event.target)) {
+        mode = "dark";
+        
+        document.documentElement.style.setProperty("color-scheme", "dark");
+        
+        darkButton.style.setProperty("background-color",
+                                     "light-dark(#686070, #706068)");
+    } else if(lightChild.includes(event.target)) {
+        mode = "light";
+        
+        document.documentElement.style.setProperty("color-scheme", "light");
+        
+        lightButton.style.setProperty("background-color",
+                                      "light-dark(#686070, #706068)");
+    } else if(autoChild.includes(event.target)) {
+        mode = "auto";
+        
+        document.documentElement.style.setProperty("color-scheme",
+                                                   "light dark");
+        
+        autoButton.style.setProperty("background-color",
+                                     "light-dark(#686070, #706068)");
+    }
+    
+    updateTheme();
+};
+
 
 updateTheme();
 
