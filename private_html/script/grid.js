8eb16b5cc52bb21ab16b1292c983f50cc94e39f7
TMu42 <11731869+TMu42@users.noreply.github.com>
2024-09-06 12:06:27 +0000

    Duplicate `/public_html` -> `/private_html` in preparation for modularizing the code and writing build scripts.

---
+++ private_html/script/grid.js
@@ -0,0 +1,121 @@
+const displayBox = document.getElementById("display-box");
+const displayImg = document.getElementById("display-image");
+const displayTtl = document.getElementById("display-title");
+const displayTxt = document.getElementById("display-text");
+
+const displayClose = document.getElementById("display-close");
+const displayLeft  = document.getElementById("display-left");
+const displayRight = document.getElementById("display-right");
+
+const grids = document.getElementsByClassName("page-grid");
+
+const gridBoxes = grids[0].getElementsByClassName("inner-box");
+
+const setGridActions = () => {
+    for(var i = 0; i < gridBoxes.length; i++) {
+        gridBoxes[i].addEventListener("click", gridClick);
+    }
+    
+    document.addEventListener("click", closeDisplay);
+    
+    displayLeft.addEventListener("click", previousDisplay);
+    displayRight.addEventListener("click", nextDisplay);
+};
+
+
+var displayGrid = gridBoxes;
+var displayElement = -1;
+
+
+function gridClick(event) {
+    displayElement = [...displayGrid].indexOf(event.currentTarget);
+    
+    if(setDisplay(displayGrid, displayElement)) {
+        displayBox.style.display = "block";
+    }
+};
+
+
+function setDisplay(set, idx) {
+    if(idx != -1) {
+        var img = set[idx].getElementsByTagName("img")[0];
+        var ttl = set[idx].getElementsByClassName("grid-text")[0];
+        var txt = set[idx].getElementsByClassName("grid-display-text")[0];
+        
+        if(typeof img !== "undefined" && img !== null) {
+            displayImg.src = img.src;
+            displayImg.alt = img.src.split('/')[img.src.split('/').length - 1];
+        } else {
+            displayImg.src = "";
+            displayImg.alt = "T&mu;"
+        }
+        
+        if(typeof ttl !== "undefined" && ttl !== null) {
+            displayTtl.innerText = ttl.innerText;
+        } else {
+            displayTtl.innerText = "";
+        }
+        
+        if(typeof txt !== "undefined" && txt !== null) {
+            displayTxt.innerHTML = txt.innerHTML;
+        } else {
+            displayTxt.innerHTML = "";
+        }
+        
+        console.log(displayTtl.innerText);
+        
+        return true;
+    }
+    
+    return false;
+};
+
+
+function nextDisplay(event) {
+    if(displayElement != -1) {
+        displayElement++;
+        
+        if(displayElement == displayGrid.length) {
+            displayElement = 0;
+        }
+        
+        setDisplay(displayGrid, displayElement);
+    }
+};
+
+
+function previousDisplay(event) {
+    if(displayElement != -1) {
+        displayElement--;
+        
+        if(displayElement == -1) {
+            displayElement = displayGrid.length - 1;
+        }
+        
+        setDisplay(displayGrid, displayElement);
+    }
+};
+
+
+function closeDisplay(event) {
+    const closeChild = [displayClose, ...getDescendants(displayClose)];
+    const displChild = [displayBox,   ...getDescendants(displayBox)];
+    
+    var gridChild = [];
+    
+    for(var i = 0; i < gridBoxes.length; i++) {
+        gridChild = [...gridChild, gridBoxes[i],
+                     ...getDescendants(gridBoxes[i])];
+    }
+    
+    if(closeChild.includes(event.target)
+    || !(displChild.includes(event.target)
+      || gridChild.includes(event.target))) {
+        displayBox.style.display = "none";
+        
+        displayElement = -1;
+    }
+};
+
+
+setGridActions();
---
+++ private_html/script/theme.js
@@ -0,0 +1,105 @@
+const favLnk = document.getElementById("favicon-link");
+
+const dark = window.matchMedia("(prefers-color-scheme: dark)");
+
+const titIm0 = document.getElementById("title-image0");
+const titIm1 = document.getElementById("title-image1");
+
+const lightButton = document.getElementById("light-button");
+const  autoButton = document.getElementById( "auto-button");
+const  darkButton = document.getElementById( "dark-button");
+
+
+lightButton.addEventListener("click", changeTheme);
+autoButton.addEventListener("click", changeTheme);
+darkButton.addEventListener("click", changeTheme);
+
+var mode;
+
+
+function detectTheme() {
+    var cookieTheme = getCookie("theme");
+    
+    if(cookieTheme === "dark") {
+        mode = "dark";
+        
+        document.documentElement.style.setProperty("color-scheme", "dark");
+        
+        darkButton.style.setProperty("background-color",
+                                     "light-dark(#686070, #706068)");
+    } else if(cookieTheme === "light") {
+        mode = "light";
+        
+        document.documentElement.style.setProperty("color-scheme", "light");
+        
+        lightButton.style.setProperty("background-color",
+                                      "light-dark(#686070, #706068)");
+    } else {
+        mode = "auto";
+        
+        document.documentElement.style.removeProperty("color-scheme");
+        
+        autoButton.style.setProperty("background-color",
+                                     "light-dark(#686070, #706068)");
+    }
+};
+
+
+function updateTheme() {
+    if(mode == "dark" || (mode == "auto" && dark.matches)) {
+        favLnk.href = "/images/emu-logo-white.png";
+        titIm0.src = "/images/emu-logo-light.png";
+        titIm1.src = "/images/emu-logo-light.png";
+    } else {
+        favLnk.href = "/images/emu-logo-black.png";
+        titIm0.src = "/images/emu-logo-dark.png";
+        titIm1.src = "/images/emu-logo-dark.png";
+    }
+};
+
+
+function changeTheme(event) {
+    const  darkChild = [ darkButton, ...getDescendants( darkButton)];
+    const  autoChild = [ autoButton, ...getDescendants( autoButton)];
+    const lightChild = [lightButton, ...getDescendants(lightButton)];
+    
+    darkButton.style.removeProperty("background-color");
+    autoButton.style.removeProperty("background-color");
+    lightButton.style.removeProperty("background-color");
+    
+    if(darkChild.includes(event.target)) {
+        mode = "dark";
+        
+        document.documentElement.style.setProperty("color-scheme", "dark");
+        
+        darkButton.style.setProperty("background-color",
+                                     "light-dark(#686070, #706068)");
+        
+        setCookie("theme", "dark");
+    } else if(lightChild.includes(event.target)) {
+        mode = "light";
+        
+        document.documentElement.style.setProperty("color-scheme", "light");
+        
+        lightButton.style.setProperty("background-color",
+                                      "light-dark(#686070, #706068)");
+        
+        setCookie("theme", "light");
+    } else if(autoChild.includes(event.target)) {
+        mode = "auto";
+        
+        document.documentElement.style.removeProperty("color-scheme");
+        
+        autoButton.style.setProperty("background-color",
+                                     "light-dark(#686070, #706068)");
+        
+        delCookie("theme");
+    }
+    
+    updateTheme();
+};
+
+detectTheme();
+updateTheme();
+
+setInterval(updateTheme, 500);
