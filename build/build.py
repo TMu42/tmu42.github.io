c1babf3a4831b43629f0b14ee2da7045776d209e
TMu42 <11731869+TMu42@users.noreply.github.com>
2024-09-08 11:57:52 +0000

    `build.py` now correctly reproduces HTML from `.template` files. compare:
                `/home.html`  with `/public_html/index.html`
                `/logos.html` with `/public_html/logos/index.html`
    
    Each `.html` in the root directory was generated by `build.py` from
    the corresponding `.template` and referenced `.fragment` files.

--- build/build.py
+++ build/build.py
@@ -11,7 +11,8 @@ ID_PARAMETRIC = "::PARAMETRIC;"
 
 COMMAND = re.compile(r"^\s*:(.*);(.*)$")   # :command; comment
 
-COM_FRAGMENT = re.compile(r"^[\w\.\-\/]+$")
+COM_FRAGMENT   = re.compile(r"^[\w\.\-\/]+$")
+COM_PARAMETRIC = re.compile(r"^([\w\.\-\/]+)\((.*)\)$")
 
 def main(args):
     if len(args) < 2 or len(args) > 3:
@@ -19,8 +20,9 @@ def main(args):
     
     try:
         infile = open(args[1])
+        inpath = '/'.join(args[1].split('/')[:-1]) + '/'
     except FileNotFoundError:
-        misuse(args[0], f"{args[0]}: file \"args[1]\" not found.")
+        misuse(args[0], f"{args[0]}: file \"args[1]\" not found")
     
     if len(args) == 3:
         outfile = open(args[2], 'w')
@@ -35,11 +37,30 @@ def main(args):
                 cmd = COMMAND.sub(r"\1", line).strip()
                 
                 if COM_FRAGMENT.match(cmd):
-                    print(f"Insert content of file({cmd}.fragment)",
-                                                            file=outfile)
+                    fragfile = None
+                    
+                    try:
+                        fragfile = open(f"{inpath}{cmd}.fragment")
+                    except FileNotFoundError:
+                        try:
+                            fragfile = open(f"{inpath}{cmd}")
+                        except FileNotFoundError:
+                            print(line, end='', file=outfile)
+                            print(f"Warning: File not found: {cmd}[.fragment]",
+                                                            file=sys.stderr)
+                    
+                    if fragfile is not None:
+                        id_line = fragfile.readline()
+                        
+                        if id_line.strip() != ID_FRAGMENT:
+                            print(id_line, end='', file=outfile)
+                            print(f"Warning: bad fragment file")
+                        
+                        for line in fragfile.readlines():
+                            print(line, end='', file=outfile)
                 else:
-                    print(line, file=outfile)
-                    print(f"Warning: unrecognized command {cmd}",
+                    print(line, end='', file=outfile)
+                    print(f"Warning: unrecognized command: {cmd}",
                                                             file=sys.stderr)
             else:
                 print(line, end='', file=outfile)
