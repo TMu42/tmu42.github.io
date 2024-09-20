6e4c924ace5aaec6b429833d24ba986b5cf8a7fb
TMu42 <11731869+TMu42@users.noreply.github.com>
2024-09-20 08:38:14 +0000

    File acquisition handled in shared function so no duplication between parser methods.

--- build/parsers.py
+++ build/parsers.py
@@ -3,13 +3,30 @@ import io
 
 
 def template_parser(tfile=None):
-    if not (isinstance(tfile, io.TextIOWrapper) or isinstance(tfile, str)):
-        raise TypeError(f"template_file() needs an open file or a string, "
-                        f"got {type(tfile)}.")
+    tfile = acquire_file(tfile, "template_parser()")
+    #if not (isinstance(tfile, io.TextIOWrapper) or isinstance(tfile, str)):
+    #    raise TypeError(f"template_file() needs an open file or a string, "
+    #                    f"got {type(tfile)}.")
+    #
+    #if isinstance(tfile, str):
+    #    tfile = open(tfile)
+    #
+    #if not tfile.readable():
+    #    raise ValueError(f"File object ({tfile.name}) is not readable (mode "
+    #                     f"is {tfile.mode!r}). Try opening with mode='r'.")
+
+
+def acquire_file(f, context="acquire_file()"):
+    if not (isinstance(f, io.TextIOWrapper) or isinstance(f, str)):
+        raise TypeError(
+            f"{context} needs an open file or a string, got {type(f)}.")
     
-    if isinstance(tfile, str):
-        tfile = open(tfile)
+    if isinstance(f, io.TextIOWrapper):
+        if not f.readable():
+            raise ValueError(
+                f"File object ({f.name}) is not readable (mode is "
+                f"{f.mode!r}). Try opening with mode='r'.")
+        
+        return f
     
-    if not tfile.readable():
-        raise ValueError(f"File object ({tfile.name}) is not readable (mode "
-                         f"is {tfile.mode!r}). Try opening with mode='r'.")
+    return open(f)
